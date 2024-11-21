efea14dd3dace3fc135d4412747000aac0e21c05
jdw5 <joshuadwallace1@gmail.com>
2024-11-21 15:22:07 +1000

    Simplifying

--- tests/Testcase.php
+++ tests/Testcase.php
@@ -1,81 +1,36 @@
 <?php
 
-namespace Honedble\Tests;
+namespace Honed\Table\Tests;
 
-use Honedble\TableServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Workbench\App\Providers\WorkbenchServiceProvider;
-
-use function Orchestra\Testbench\workbench_path;
+use Honed\Table\TableServiceProvider;
 
 class TestCase extends Orchestra
 {
     protected function setUp(): void
     {
         parent::setUp();
-        $this->artisan('migrate:fresh', ['--database' => 'testing']);
-        $this->artisan('db:seed', ['--database' => 'testing']);
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Honed\\Table\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            WorkbenchServiceProvider::class,
             TableServiceProvider::class,
         ];
     }
 
-    protected function defineDatabaseMigrations()
-    {
-        $this->loadMigrationsFrom(workbench_path('database/migrations'));
-    }
-
     public function getEnvironmentSetUp($app)
     {
-        $app['config']->set('app.key', 'base64:'.base64_encode(random_bytes(32)));
+        config()->set('database.default', 'testing');
 
-        $app['config']->set('database.connections.testing', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-
-        $app['config']->set('workbench', [
-            'start' => '/',
-            'install' => true,
-            'guard' => 'web',
-            'discovers' => [
-                'web' => true,
-                'api' => false,
-                'commands' => false,
-                'components' => false,
-                'views' => false,
-            ],
-            'build' => [
-                'create-sqlite-db',
-                'migrate:fresh',
-            ],
-            'assets' => [],
-            'sync' => [],
-        ]);
-
-        // $app['config']->set('inertia', [
-        //     'testing' => [
-        //         'ensure_pages_exist' => false,
-        //         'page_paths' => [],
-        //         'page_extensions' => [],
-        //     ],
-        // ]);
-    }
-
-    /**
-     * Define routes setup.
-     *
-     * @param  \Illuminate\Routing\Router  $router
-     * @return void
-     */
-    protected function defineRoutes($router)
-    {
-        require workbench_path('routes/web.php');
+        /*
+        $migration = include __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
+        $migration->up();
+        */
     }
 }
